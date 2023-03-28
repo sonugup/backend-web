282959185dad40a1428974dd3d12c58fbf8a8733
Sonu Gupta <101567095+sonugup@users.noreply.github.com>
2023-03-28 23:35:46 +0530

    Update Note.route.js

--- Authentication/routes/Note.route.js
+++ Authentication/routes/Note.route.js
@@ -13,7 +13,7 @@ noteRoute.post("/create", async (req, res) => {
   try {
     const n_notes = new NoteModel(payload)
     await n_notes.save()
-    res.send("add the note");
+    res.send("add the note")
   } catch (err) {
     console.log(err);
     res.send({ msg: "Somthing wrong" });
